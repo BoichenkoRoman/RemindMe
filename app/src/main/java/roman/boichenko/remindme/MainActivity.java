36a93e0433b3fae34aad7435be30bc4f370619e8
BoichenkoRoman <boichenko.roman@gmail.com>
2018-10-20 16:39:33 +0300

    коментарий

--- app/src/main/java/roman/boichenko/remindme/MainActivity.java
+++ app/src/main/java/roman/boichenko/remindme/MainActivity.java
@@ -1,27 +1,30 @@
 package roman.boichenko.remindme;
 
-import android.app.Activity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
-private Toolbar toolbar;
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault); //  указали какая тема по умолчанию
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /* */
-        
-        
+
+
         initToolbar();
-        
-        
-        
+
+
     }
 
     private void initToolbar() {
-        toolbar =  (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
 
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
